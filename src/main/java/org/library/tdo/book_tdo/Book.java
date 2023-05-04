73eca8a398ce95d452966ce0c8c462d66d07ee55
V3ss3l <grishav3ss3l@gmail.com>
2023-05-04 13:14:27 +0400

    Little changes 04/05

--- src/main/java/org/library/tdo/book_tdo/Book.java
+++ src/main/java/org/library/tdo/book_tdo/Book.java
@@ -2,8 +2,10 @@ package org.library.tdo.book_tdo;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import lombok.Data;
+import org.library.tdo.Adress;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -22,4 +24,14 @@ public class Book extends PanacheEntity {
     private String language;
     private String annotation;
     private String additionalInfo;
+
+    public static List<Book> findByName(String name){
+        return find("bookName = ?1", name).list();
+    }
+    public static List<Book> findByAuthor(String author){
+        return find("author = ?1", author).list();
+    }
+    public static List<Book> findByPublisher(String publisherName){
+        return find("pubisher = ?1", publisherName).firstResult();
+    }
 }
